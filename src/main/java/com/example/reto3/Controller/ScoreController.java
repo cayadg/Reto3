3bcfc63dc9399429c2fec90773acaf5fe4b09d62
cayadg <claudia.moreno.at@usa.edu.co>
2023-05-23 16:05:35 -0500

    primer commit
    descripcion de prueba

---
+++ src/main/java/com/example/reto3/Controller/ScoreController.java
@@ -0,0 +1,34 @@
+package com.example.reto3.Controller;
+
+import com.example.reto3.Model.Score;
+import com.example.reto3.Service.ScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@RestController
+@RequestMapping("/api/Score")
+public class ScoreController {
+    @Autowired
+    private ScoreService scoreService;
+
+    @GetMapping("/all")
+    public List<Score> getAll(){
+        return scoreService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Score> getScore (@PathVariable int id){
+        return scoreService.getScore(id);
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Score save(@RequestBody Score score){
+        return scoreService.save(score);
+    }
+}
