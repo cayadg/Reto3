2d179c6b211b19276654ec0746091cc2437fa330
cayadg <claudia.moreno.at@usa.edu.co>
2023-05-26 21:26:00 -0500

    otra vez

--- src/main/java/com/example/reto3/Controller/UserController.java
+++ src/main/java/com/example/reto3/Controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.reto3.Controller;
 
-import org.hibernate.mapping.Collection;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
