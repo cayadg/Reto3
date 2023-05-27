0f65ab3043beacccc26dfaa0cc5c9fc8c1fc7922
cayadg <claudia.moreno.at@usa.edu.co>
2023-05-26 20:34:25 -0500

    actual html

--- src/main/java/com/example/reto3/Security/SecurityAdapter.java
+++ src/main/java/com/example/reto3/Security/SecurityAdapter.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableWebSecurity
-public class SecurityAdapter extends WebSecurityConfigurerAdapter{
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
   @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(
                 a -> a.antMatchers("/", "/error", "/webjars/**", "/api/**").permitAll().anyRequest().authenticated()
                 ).exceptionHandling(
